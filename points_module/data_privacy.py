0b5201ed3c4f74dcd72203b1ee97e324a22c313b
c8garcia-T <18cagarcia@gmail.com>
2024-01-06 16:52:15 +0900

    removing pycache tracking

--- points_module/data_privacy.py
+++ points_module/data_privacy.py
@@ -1,14 +1,13 @@
 # Objective: Anonymize Student and Teacher Names
 # currently this function only anonymizes teacher names since student is anonymized form source
 import pandas as pd
-import numpy as np
+from random import shuffle
 
 
 def anonymize_student_n_teacher(df):
     teacher_names = df["teacher"].unique()
-    random_ids = set(
-        np.random.randint(low=0, high=len(teacher_names) * 10, size=len(teacher_names))
-    )
+    random_ids = [i for i, _ in enumerate(teacher_names)]
+    shuffle(random_ids)
     mapping_ids = {
         teacher: f"ID_{id_rand}" for id_rand, teacher in zip(random_ids, teacher_names)
     }
--- tests/test_data_wrangling.py
+++ tests/test_data_wrangling.py
@@ -1,8 +1,6 @@
-import pytest
 import numpy as np
 import os
 import sys
-import pandas as pd
 
 sys.path.append(os.getcwd())
 
