a4d7b7830890424918901bd970a48c202e738506
c8garcia-T <18cagarcia@gmail.com>
2024-01-07 14:13:48 +0900

    data for streamlit

---
+++ make_public_data.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import os
+from random import shuffle
+
+
+# Define a function to apply to the second column
+def modify_column(column):
+    teacher_names = column.unique()
+    random_ids = [i for i, _ in enumerate(teacher_names)]
+    shuffle(random_ids)
+    mapping_ids = {
+        teacher: f"ID_{id_rand}" for id_rand, teacher in zip(random_ids, teacher_names)
+    }
+    column = column.map(mapping_ids)
+
+    return column
+
+
+if "__name__" == "__name__":
+    raw_data_path = "points_raw_data_by_class/points_data_vicky_t.xlsx"
+    dfs = pd.read_excel(raw_data_path, sheet_name=None, header=None)
+    # Modify the fifth column in each DataFrame
+    column_to_modify = 4  # Assuming Python indexing (0-based), selects the fifth column
+    for sheet_name, df in dfs.items():
+        if len(df.columns) > column_to_modify:
+            df.iloc[:, column_to_modify] = modify_column(df.iloc[:, column_to_modify])
+
+    # Write the modified DataFrames back to a new Excel file
+    output_excel_file_path = os.path.join(
+        "data_public_ok", os.path.basename(raw_data_path)
+    )
+    with pd.ExcelWriter(output_excel_file_path, engine="xlsxwriter") as writer:
+        for sheet_name, df in dfs.items():
+            df.to_excel(writer, sheet_name=sheet_name, index=False, header=None)
+
+    print(
+        f"Excel file '{output_excel_file_path}' has been created with modified columns."
+    )
--- report.py
+++ report.py
@@ -21,7 +21,7 @@ from streamlit_module.sections import (
 )
 
 # Data Source
-data_source = "points_raw_data_by_class/points_data_vicky_t.xlsx"
+data_source = "data_public_ok/points_data_vicky_t.xlsx"
 # Preliminary Dataset Builds
 points_df = build_clean_df(data_source)
 aggregated_data_by_student_df = build_aggregated_data_by_student_df(data_source)
